6a8f8ff10b18892f415d4ea3e5193aeccbc91d1a
daldunin <d_aldunin@mail.ru>
2015-12-10 22:19:02 +0300

    Optimized metrics output

--- ClassificationMain.R
+++ ClassificationMain.R
@@ -21,7 +21,7 @@ print(sum(predictedLabels == trainLabels)/length(trainLabels))
 
 #calculate the following error metric for each class obtained on the train data:
 #Recall, precision, specificity, F-measure, FDR and ROC for each class separately. Use a package for ROC. 
-
+calcAndShowMetrics(predictedLabels,trainLabels)
 
 # test the model
 data <- loadMNISTData("D:\\LearnMNIST\\LearnMNIST\\t10k-images.idx3-ubyte", "D:\\LearnMNIST\\LearnMNIST\\t10k-labels.idx1-ubyte")
@@ -41,37 +41,4 @@ print(sum(predictedLabels == testLabels)/length(testLabels))
 
 #calculate the following error metric for each class obtained on the test data:
 #Recall, precision, specificity, F-measure, FDR and ROC for each class separately. Use a package for ROC. 
-TP <- 0 * 1:10  # True Positive
-TN <- 0 * 1:10  # True Negative
-FP <- 0 * 1:10  # Fasle Positive
-FN <- 0 * 1:10  # False Negative
-
-for (i in 1:10){
-  TP[i] <- sum((testLabels == (i-1)) * (predictedLabels== (i-1)))
-  TN[i] <- sum((testLabels != (i-1)) * (predictedLabels != (i-1)))
-  FP[i] <- sum((testLabels != (i-1)) * (predictedLabels == (i-1)))
-  FN[i] <- sum((testLabels == (i-1)) * (predictedLabels != (i-1)))
-  plot(roc(as.numeric((testLabels==(i-1))), as.numeric(predictedLabels==(i-1))))
-}
-
-# Calculate error metrics
-
-# recall
-accuracy <- (TP+TN)/(TP+TN+FP+FN)
-print(accuracy)
-
-# precision
-precision <- TP/(TP+FP)
-print(precision)
-
-# specificity
-specificity <- TP/(TP+FN)
-print(specificity)
-
-# F-measure
-fmeasure <- 2*TP/(2*TP+FP+FN)
-print(fmeasure)
-
-# FDR
-FDR <- FP/(FP+TP)
-print(FDR)
+calcAndShowMetrics(predictedLabels,testLabels)
--- tools.R
+++ tools.R
@@ -4,7 +4,7 @@ learnModel <- function(data, labels){
   result <- matrix(data = 0, nrow = 10, ncol = ncol(data) + 1, byrow = T)
   initial_theta <- 0 * 1 : (ncol(data) + 1)
   for (i in 1:10){
-    print(i)
+    print(i - 1)
     buff <- optim(initial_theta, logRegCostFunction, gr = gradLinRegCostFunction,
     	data=cbind(1, data), labels = (labels == (i - 1)), lambda = lambda, method = "CG")
     result[i,] <- buff$par
@@ -36,4 +36,41 @@ sFunc <- function(arg){
   # Returns Sigma 
   sigma <- 1 / (1 + exp(-arg))
   return(sigma)
+}
+
+calcAndShowMetrics <- function(predictedLabels,testLabels){
+#calculate the following error metrics:
+#Recall, precision, specificity, F-measure, FDR and ROC
+TP <- 0 * 1:10  # True Positive
+TN <- 0 * 1:10  # True Negative
+FP <- 0 * 1:10  # Fasle Positive
+FN <- 0 * 1:10  # False Negative
+
+for (i in 1:10){
+  TP[i] <- sum((testLabels == (i-1)) * (predictedLabels== (i-1)))
+  TN[i] <- sum((testLabels != (i-1)) * (predictedLabels != (i-1)))
+  FP[i] <- sum((testLabels != (i-1)) * (predictedLabels == (i-1)))
+  FN[i] <- sum((testLabels == (i-1)) * (predictedLabels != (i-1)))
+  plot(roc(as.numeric((testLabels==(i-1))), as.numeric(predictedLabels==(i-1))))
+}
+
+# recall
+accuracy <- (TP+TN)/(TP+TN+FP+FN)
+print(accuracy)
+
+# precision
+precision <- TP/(TP+FP)
+print(precision)
+
+# specificity
+specificity <- TP/(TP+FN)
+print(specificity)
+
+# F-measure
+fmeasure <- 2*TP/(2*TP+FP+FN)
+print(fmeasure)
+
+# FDR
+FDR <- FP/(FP+TP)
+print(FDR)
 }
